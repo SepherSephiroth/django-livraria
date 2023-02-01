68f1145225caffa09f488ca28475fc58fd6e568a
Bill Harford <bharford@exemple.com>
2023-02-01 01:19:48 -0300

    Create new view CategoriasView in core/views.py

--- core/views.py
+++ core/views.py
@@ -1,5 +1,19 @@
+import json
+
 from django.http import HttpResponse
+from django.utils.decorators import method_decorator
+from django.views import View
+from django.views.decorators.csrf import csrf_exempt
+
+from core.models import Categoria
 
 
 def teste(request):
   return HttpResponse('Olá mundo do Django')
+
+@method_decorator(csrf_exempt, name='dispatch')
+class CategoriaView(View):
+  def get(self, request):
+    data = list(Categoria.objects.values())
+    formated_data = json.dumps(data, ensure_ascii=False)
+    return HttpResponse(formated_data, content_type='application/json')
--- livraria/urls.py
+++ livraria/urls.py
@@ -20,5 +20,6 @@ from core import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.teste)
+    path('', views.teste),
+    path('categorias/', views.CategoriaView.as_view())
 ]
