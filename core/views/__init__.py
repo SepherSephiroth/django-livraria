e2935006d5a4e5314a26faaedc2a521cc3593059
Bill Harford <bharford@exemple.com>
2023-02-06 00:20:24 -0300

    Split views into multiple files inside core/views

---
+++ core/views/__init__.py
@@ -0,0 +1,5 @@
+from .categoria import CategoriaViewSet
+from .categoriaApiView import CategoriaDetail, CategoriaList
+from .categoriaClass import CategoriaView
+from .categoriaGeneric import CategoriaDetailGeneric, CategoriasListGeneric
+from .testesIniciais import teste
---
+++ core/views/categoriaGeneric.py
@@ -0,0 +1,15 @@
+from rest_framework.generics import (ListCreateAPIView,
+                                     RetrieveUpdateDestroyAPIView)
+
+from core.models import Categoria
+from core.serializers import CategoriaSerializer
+
+
+class CategoriasListGeneric(ListCreateAPIView):
+  queryset = Categoria.objects.all()
+  serializer_class = CategoriaSerializer
+
+class CategoriaDetailGeneric(RetrieveUpdateDestroyAPIView):
+  lookup_field = 'id'
+  queryset = Categoria.objects.all()
+  serializer_class = CategoriaSerializer
