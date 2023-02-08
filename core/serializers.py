1a46a94ee4ca3a40d866fd2225d37a926f5f9b93
Bill Harford <bharford@exemple.com>
2023-02-08 17:09:56 -0300

    Create ItensCompraSerializer and present more information about the status of compra

--- core/serializers.py
+++ core/serializers.py
@@ -2,7 +2,7 @@ from django.contrib.auth.models import User
 from rest_framework.serializers import (CharField, ModelSerializer,
                                         SerializerMethodField)
 
-from .models import Autor, Categoria, Compra, Editora, Livro
+from .models import Autor, Categoria, Compra, Editora, Livro, ItensCompra
 
 
 class CategoriaSerializer(ModelSerializer):
@@ -50,8 +50,25 @@ class LivroDetailSerializer(ModelSerializer):
 
 # Compra
 
+class ItensCompraSerializer(ModelSerializer):
+  total = SerializerMethodField()
+  class Meta:
+    model = ItensCompra
+    fields = ('livro', 'quantidade', 'total')
+    depth = 1
+
+  def get_total(self, obj):
+    return obj.quantidade * obj.livro.preco
+    
+
 class CompraSerializer(ModelSerializer):
   usuario = CharField(source='usuario.email')
+  status = SerializerMethodField()
+  itens = ItensCompraSerializer(many=True)
   class Meta:
     model = Compra
     fields = '__all__'
+    
+  
+  def get_status(self, obj):
+    return obj.get_status_display()
