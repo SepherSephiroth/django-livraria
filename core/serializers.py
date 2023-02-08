799fc6e7e754d26984d7792123472d4973b9d978
Bill Harford <bharford@exemple.com>
2023-02-07 23:13:59 -0300

    Present livros in three different ways using CharField, NestedSerializer and SerializerMethodField

--- core/serializers.py
+++ core/serializers.py
@@ -1,4 +1,5 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import (CharField, ModelSerializer,
+                                        SerializerMethodField)
 
 from .models import Autor, Categoria, Editora, Livro
 
@@ -13,6 +14,11 @@ class EditoraSerializer(ModelSerializer):
     model = Editora
     fields = '__all__'
 
+class EditoraNestedSerializer(ModelSerializer):
+  class Meta:
+    model = Editora
+    fields = ('nome',)
+
 class AutorSerializer(ModelSerializer):
   class Meta:
     model = Autor
@@ -24,7 +30,19 @@ class LivroSerializer(ModelSerializer):
     fields = '__all__'
 
 class LivroDetailSerializer(ModelSerializer):
+  categoria = CharField(source='categoria.descricao')
+  editora = EditoraNestedSerializer()
+  autores = SerializerMethodField()
+  
   class Meta:
     model = Livro
     fields = '__all__'
     depth = 1
+
+  def get_autores(self, obj):
+   nomes_autores = []
+   autores = obj.autores.get_queryset() 
+   for autor in autores:
+    nomes_autores.append(autor.nome)
+   
+   return nomes_autores
